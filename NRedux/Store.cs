1b7d99176139af1095a36292c883254b14a08e04
Aaron Roberts <lilasquared@gmail.com>
2017-03-25 04:13:51 -0400

    initial commit

---
+++ NRedux/Store.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NRedux {
+    public class Store<TState> {
+        public Store(Func<Object, Object> dispatch, Func<Action, Action> subscribe, Func<TState> getState, Action<Reducer<TState>> replaceReducer) {
+            GetState = getState;
+            Subscribe = subscribe;
+            Dispatch = dispatch;
+            ReplaceReducer = replaceReducer;
+        }
+        public Func<TState> GetState { get; }
+        public Func<Action, Action> Subscribe { get; }
+        public Func<Object, Object> Dispatch { get; }
+        public Action<Reducer<TState>> ReplaceReducer { get; }
+    }
+}
