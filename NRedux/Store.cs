7622a18520d8063c9a1d65a14bd7dad27eec8ba5
Aaron Roberts <lilasquared@gmail.com>
2017-03-25 04:23:12 -0400

    Add IStore interface

--- NRedux/Store.cs
+++ NRedux/Store.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 
 namespace NRedux {
-    public class Store<TState> {
+    public interface IStore<TState> {
+        Func<TState> GetState { get; }
+        Func<Action, Action> Subscribe { get; }
+        Func<Object, Object> Dispatch { get; }
+        Action<Reducer<TState>> ReplaceReducer { get; }
+    }
+
+    public class Store<TState> : IStore<TState> {
         public Store(Func<Object, Object> dispatch, Func<Action, Action> subscribe, Func<TState> getState, Action<Reducer<TState>> replaceReducer) {
             GetState = getState;
             Subscribe = subscribe;
